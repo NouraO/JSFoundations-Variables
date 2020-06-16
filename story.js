eb6cb001587419bb940393f15724c0619443156e
Noura Al-Obaidly <Engineer.Nuwiin@Nouras-MacBook.local>
2020-06-16 17:03:29 +0300

    Variables Task

--- story.js
+++ story.js
@@ -5,3 +5,12 @@
 // Define the hobby here on line 6
 
 // Print the story here on line 8
+let name = "Noura Al-Obaidly";
+const age = 27;
+const hobby = "meditation";
+
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old . By then you'll have decades of ${hobby} practice!"`
+);
